e92767e6b070c0f5f413c2c0049d10e71df10602
bioumaiquan <shengwanle@gmail.com>
2019-05-13 14:36:17 +0800

    little fix to the dungeon scene about lights

---
+++ TA_Demo/Assets/My3rdPerson/Scripts/LightScaler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightScaler : MonoBehaviour
+{
+    public bool scale = false;
+    Light[] lights = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scale == true)
+        {
+            lights = null;
+            lights = GetComponentsInChildren<Light>();
+
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].range *= gameObject.transform.localScale.x;
+            }
+
+            scale = false;
+        }
+    }
+}
